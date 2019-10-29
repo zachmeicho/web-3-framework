65914e28ece76b657a6621b6c21b3e1f7803163e
Zachary Meicho <zmeicho@lab-ceb131-m12.eastern.ewu.edu>
2019-10-29 10:55:27 -0700

    Base commit
    changed background color

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Web 3 Framework
         </a>
       </header>
     </div>
